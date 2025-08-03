3f4973f73c915d39f6c4d925ec6f27be726dae01
Aaron Meinel <aaron.meinel@procura-inventory.com>
2025-08-03 20:56:27 +0200

    feat: make simple crud for constituent work

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
+
+import GHC.Generics (Generic)
+import Data.Aeson (FromJSON, ToJSON)
+import Servant (ReqBody, FormUrlEncoded, (:>), (:<|>)(..), Post, Get, Proxy(..), Server, Handler)
+import qualified Data.Text as T
+import Web.FormUrlEncoded (FromForm)
+
 
 import App.Env (getDbConnectInfo)
 import App.ConstituentRepoPostgres ()
@@ -25,29 +33,40 @@ import Control.Monad.Logger (runNoLoggingT)
 import Control.Monad.Trans.Resource (runResourceT)
 import App.ConstituentRepoPostgres (migrateAll)
 
+data AddForm = AddForm { name :: String, email :: String } deriving (Show, Generic)
 
+instance FromForm AddForm
 
--- API definition
-type CRMApi = "constituents" :> Get '[HTML] (Html ())
-           :<|> "add" :> QueryParam' '[Required] "name" String
-                        :> QueryParam' '[Required] "email" String
-                        :> Post '[HTML] (Html ())
-
-crmServer :: Server CRMApi
-crmServer = listHandler :<|> addHandler
-  where
-    listHandler = do
-      cs <- liftIO Repo.listConstituents
-      pure $ renderHtml cs
-    addHandler name email = do
-      _ <- liftIO $ Repo.addConstituent (Constituent name email)
-      cs <- liftIO Repo.listConstituents
-      pure $ renderHtml cs
 
 renderHtml :: [Constituent] -> Html ()
 renderHtml cs = html_ $ body_ $ do
   h1_ "Constituents"
-  ul_ $ mapM_ (\c -> li_ (toHtml (name c <> " (" <> email c <> ")"))) cs
+  form_ [method_ "post", action_ "/add"] $ do
+    label_ [for_ "name"] "Name: "
+    input_ [type_ "text", id_ "name", name_ "name"]
+    label_ [for_ "email"] " Email: "
+    input_ [type_ "email", id_ "email", name_ "email"]
+    button_ [type_ "submit"] "Add Constituent"
+  ul_ $ mapM_ (\c -> li_ (toHtml (Domain.Constituent.name c <> " (" <> Domain.Constituent.email c <> ")"))) cs
+
+
+-- API definition
+type CRMApi = Get '[HTML] (Html ())
+           :<|> "constituents" :> Get '[HTML] (Html ())
+           :<|> "add" :> ReqBody '[FormUrlEncoded] AddForm :> Post '[HTML] (Html ())
+
+crmServer :: Server CRMApi
+crmServer = rootHandler :<|> listHandler :<|> addHandler
+  where
+    rootHandler = listHandler
+    listHandler = do
+      cs <- liftIO Repo.listConstituents
+      pure $ renderHtml cs
+    addHandler (AddForm name email) = do
+      _ <- liftIO $ Repo.addConstituent (Constituent name email)
+      cs <- liftIO Repo.listConstituents
+      pure $ renderHtml cs
+
 
 crmApi :: Proxy CRMApi
 crmApi = Proxy
